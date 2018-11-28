3007d0c616b35e339fb02810784f5951bc995ef5
Barbara <bmonedeiro@gmail.com>
2018-11-28 19:16:56 -0200

    saving token

---
+++ pages/callback.vue
@@ -0,0 +1,56 @@
+<template>
+  <section class="container">
+    {{token}}
+  </section>
+</template>
+
+<script>
+import {mapMutations, mapGetters, mapActions} from 'vuex'
+
+export default {
+    computed: {
+        ...mapGetters([
+            'token',
+        ])
+    },
+    methods : {
+        ...mapMutations(['SET_TOKEN'])
+    },
+    mounted () {
+        this.SET_TOKEN(this.$route.query.code)
+    }
+}
+</script>
+
+<style>
+
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
--- store/index.js
+++ store/index.js
@@ -1,27 +1,26 @@
 import Vuex from 'vuex'
 import axios from 'axios'
 
-
 const createStore = () => {
+
   return new Vuex.Store({
     state: () => ({
-      counter: 0
+      token: null
     }),
+
     mutations: {
-      increment (state) {
-        state.counter++
+      SET_TOKEN (state, payload) {
+        console.log(payload)
+        state.token = payload
       }
     },
+    
     actions: {
-        getAuth ({params}) {
-            return axios.get(`http://localhost:3000/teste`)
-            .then((res) => {
-                console.log(res)
-                return {
-                    title: res
-                }
-            })
-        }
+        
+    },
+
+    getters:{
+      token: state => state.token
     }
   })
 }
